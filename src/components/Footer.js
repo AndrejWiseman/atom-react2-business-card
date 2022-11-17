8ae55173cb86b0bfd671071a3bb4715529696102
A.M <andrej2000aa@gmail.com>
2022-11-17 19:18:42 +0100

    dodan insta link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,9 +12,9 @@ function Footer() {
             <button><img src={ twitter } alt="social"/></button>
             <button><img src={ facebook } alt="social"/></button>
 
-            <a href="instagram://user?username=mudri.andrej">instaaa</a>
+            <a href="instagram://user?username=mudri.andrej" className='instalink'><img src={ insta } alt="social"/></a>
             
-            <button><img src={ insta } alt="social"/></button>
+            {/*<button><img src={ insta } alt="social"/></button>*/}
             <button><img src={ linked } alt="social"/></button>
             <button><img src={ github } alt="social"/></button>
         </div>
